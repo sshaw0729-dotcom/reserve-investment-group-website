03635a2f0be5fdf48e2e7fa59b7f751532ec7748
sshaw0729-dotcom <sshaw0729@gmail.com>
2026-08-16 03:54:27 -0400

    Assemble reference RIG artwork asset

---
+++ src/lib/brand/referenceRigArtwork.ts
@@ -0,0 +1,8 @@
+import chunk0 from "./referenceRigArtwork/chunk0";
+import chunk1 from "./referenceRigArtwork/chunk1";
+import chunk2 from "./referenceRigArtwork/chunk2";
+import chunk3 from "./referenceRigArtwork/chunk3";
+import chunk4 from "./referenceRigArtwork/chunk4";
+
+/** Exact RIG visual supplied in the approved reference package, encoded as WebP. */
+export const referenceRigArtwork = `data:image/webp;base64,${chunk0}${chunk1}${chunk2}${chunk3}${chunk4}`;
